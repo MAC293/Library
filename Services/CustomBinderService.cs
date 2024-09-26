aeb337a6699256d2511c4222a5a8561da2cec423
Joseph Pino <mac110973@gmail.com>
2024-09-26 20:49:14 -0300

    Custom Binder, Done

--- Services/CustomBinderService.cs
+++ Services/CustomBinderService.cs
@@ -5,35 +5,72 @@ using System.Text.Json;
 
 namespace Library.Services
 {
+    //public class CustomBinderService : IModelBinder
+    //{
+    //    public async Task BindModelAsync(ModelBindingContext bindingContext)
+    //    {
+    //        if (bindingContext != null)
+    //        {
+    //            var form = bindingContext.HttpContext.Request.Form;
+    //            Log.Information("form: {@form}", form);
+
+    //            var bookJSON = form["newBook"];
+    //            Log.Information("bookJSON: {@bookJSON}", bookJSON);
+
+    //            var book = JsonSerializer.Deserialize<Book>(bookJSON);
+    //            Log.Information("book: {@book}", book);
+
+    //            var file = form.Files.GetFile("newCover");
+    //            Log.Information("file: {@file}", file);
+
+    //            if (file != null)
+    //            {
+    //                using (var memoryStream = new MemoryStream())
+    //                {
+    //                    await file.CopyToAsync(memoryStream);
+    //                    book.Cover = memoryStream.ToArray();
+    //                }
+    //            }
+
+    //            bindingContext.Result = ModelBindingResult.Success(book);
+    //        }
+    //    }
+    //}
+
     public class CustomBinderService : IModelBinder
     {
         public async Task BindModelAsync(ModelBindingContext bindingContext)
         {
-            if (bindingContext != null)
+            if (bindingContext == null) return;
+
+            var form = bindingContext.HttpContext.Request.Form;
+            //Log.Information("form: {@form}", form);
+
+            var bookJSON = form["newBook"];
+            //Log.Information("bookJSON: {@bookJSON}", bookJSON);
+
+            var options = new System.Text.Json.JsonSerializerOptions
             {
-                var form = bindingContext.HttpContext.Request.Form;
-                Log.Information("form: {@form}", form);
+                PropertyNameCaseInsensitive = true
+            };
 
-                var bookJSON = form["newBook"];
-                Log.Information("bookJSON: {@bookJSON}", bookJSON);
+            var book = System.Text.Json.JsonSerializer.Deserialize<Book>(bookJSON, options);
+            Log.Information("book: {@book}", book);
 
-                var book = JsonSerializer.Deserialize<BookBorrowService>(bookJSON);
-                Log.Information("book: {@book}", book);
+            var file = form.Files.GetFile("newCover");
+            //Log.Information("file: {@file}", file);
 
-                var file = form.Files.GetFile("newCover");
-                Log.Information("file: {@file}", file);
-
-                if (file != null)
+            if (file != null)
+            {
+                using (var memoryStream = new MemoryStream())
                 {
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        await file.CopyToAsync(memoryStream);
-                        book.Cover = memoryStream.ToArray();
-                    }
+                    await file.CopyToAsync(memoryStream);
+                    book.Cover = memoryStream.ToArray();
                 }
-
-                bindingContext.Result = ModelBindingResult.Success(book);
             }
+
+            bindingContext.Result = ModelBindingResult.Success(book);
         }
     }
+
 }
